3602cb9474044e2ec33e580308508ddbfaa51248
Betselot Bezuayehu <king.betse@gmail.com>
2024-04-21 10:29:33 +0300

    user model done

--- lib/models/user_mode.dart
+++ lib/models/user_mode.dart
@@ -1,49 +1,64 @@
 import 'package:equatable/equatable.dart';
-import 'package:ergata/entities/patient_entity.dart';
+import 'package:ergata/entities/patiententity.dart';
 
 class MyPatient extends Equatable {
   final String id;
   final String phone;
   final String age;
   final bool isDiagnosed;
+  final String gender;
   final String diagnosedWith;
 
   const MyPatient({
     required this.id,
     required this.phone,
     required this.age,
+        required this.gender,
+
     required this.isDiagnosed,
     required this.diagnosedWith,
-
-
   });
 
   toJson() {
-    return {'id': id, 'phone': phone, 'age': age, 'isDiagnosed': isDiagnosed, 'diagnosedWith': diagnosedWith};
+    return {
+      'id': id,
+      'phone': phone,
+      'age': age,
+      'gender':gender,
+      'isDiagnosed': isDiagnosed,
+      'diagnosedWith': diagnosedWith
+    };
   }
 
-  static final empty = MyPatient(id: "", phone: "", age: "",  isDiagnosed:false,diagnosedWith: "");
+  static final empty = MyPatient(
+      id: "", phone: "", age: "",gender:"" ,isDiagnosed: false, diagnosedWith: "");
 
   MyPatient copyWith({
     String? id,
     String? phone,
     String? age,
     String? photo,
-    bool ? isDiagnosed,
+    String? gender,
+    bool? isDiagnosed,
     String? diagnosedWith,
-
   }) {
     return MyPatient(
         id: id ?? this.id,
         age: age ?? this.age,
         phone: phone ?? this.phone,
-        isDiagnosed: isDiagnosed?? this.isDiagnosed,
-        diagnosedWith: diagnosedWith??this.diagnosedWith
-        );
+        gender: gender??this.gender,
+        isDiagnosed: isDiagnosed ?? this.isDiagnosed,
+        diagnosedWith: diagnosedWith ?? this.diagnosedWith);
   }
 
   PatientEntity toEntity() {
-    return PatientEntity(id: id, phone: phone,  age: age, isDiagnosed: isDiagnosed,diagnosedWith: diagnosedWith);
+    return PatientEntity(
+        id: id,
+        age: age,
+        phone: phone,
+        gender: gender,
+        isDiagnosed: isDiagnosed,
+        diagnosedWith: diagnosedWith);
   }
 
   static MyPatient fromEntity(PatientEntity entity) {
@@ -51,11 +66,12 @@ class MyPatient extends Equatable {
       id: entity.id,
       age: entity.age,
       phone: entity.phone,
+      gender: entity.gender,
       isDiagnosed: entity.isDiagnosed,
       diagnosedWith: entity.diagnosedWith,
     );
   }
 
   @override
-  List<Object?> get props => [id, phone, age, isDiagnosed,diagnosedWith ];
+  List<Object?> get props => [id, phone, age, isDiagnosed, diagnosedWith];
 }
